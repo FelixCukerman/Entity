8f44f742b44c180e561908d0b9cbfe5bae84d351
FelixCukerman <andreyka.abiev@gmail.com>
2018-07-15 00:02:50 +0300

    Add project files.

---
+++ ClassLibrary1/Models/Aviator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HometaskEntity.DAL.Models
+{
+    public class Aviator
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public int Experience { get; set; }
+    }
+}
---
+++ ClassLibrary1/Models/Crew.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HometaskEntity.DAL.Models
+{
+    public class Crew
+    {
+        public int Id { get; set; }
+        public Aviator aviator { get; set; }
+        public List<Stewardess> stewardesses { get; set; }
+    }
+}
