7b060ed069d907166c27226a4d88dee9f71f2f4f
FelixCukerman <andreyka.abiev@gmail.com>
2018-07-15 00:17:57 +0300

    add database

---
+++ ClassLibrary1/AirportContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using HometaskEntity.DAL.Models;
+
+namespace homework_5_bsa2018.DAL
+{
+    public class AirportContext : DbContext
+    {
+        public DbSet<Aviator> Aviators { get; set; }
+        public DbSet<Crew> Crews { get; set; }
+        public DbSet<Departure> Departures { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Stewardess> Stewardesses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TypePlane> TypesPlane { get; set; }
+
+        public AirportContext(DbContextOptions<AirportContext> contextOptions) : base(contextOptions)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
